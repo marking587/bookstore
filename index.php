2ed8a3dcac8af7b6db47a656e6829ed8cac918e1
Robert Kestel <robertkestel@gmx.de>
2017-02-01 09:10:02 +0100

    bug fixing - login/register

--- index.php
+++ index.php
@@ -49,11 +49,11 @@ include_once './api/books.php';
 <nav class="navbar navbar-inverse">
     <div class="container-fluid">
         <div class="navbar-header">
-            <!--button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
-            </button-->
+            </button>
             <span class="navbar-brand" >G10</span>
         </div>
         <div class="collapse navbar-collapse" id="myNavbar">
